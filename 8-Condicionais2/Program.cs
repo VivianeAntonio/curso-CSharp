b893f6536623c1274ecbb4fca2ef5a11f9df2937
Viviane Antonio <asilvaviviane@gmail.com>
2021-11-17 20:15:56 -0300

    condicionais 2 com operadores logicos

---
+++ 8-Condicionais2/Program.cs
@@ -0,0 +1,34 @@
+﻿//operador OU ||
+//operador E &&
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Condicionais2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 8 - Condicionais 2 ");
+
+            int idadeJoao = 10;
+            int quantidadePessoas = 3;
+                        
+            if (idadeJoao >= 18 || quantidadePessoas >= 2)
+            {
+                Console.WriteLine("Pode entrar.");
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar.");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
