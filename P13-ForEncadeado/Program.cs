f84b88c7387602c28ce9d713354a7b7bce216c64
VivianeAntonio <asilvaviviane@gmail.com>
2021-11-22 18:55:29 -0300

    exercises laços

---
+++ P13-ForEncadeado/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P13_ForEncadeado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 13 - For encadeado");
+
+            //Este código vai executar o seguinte desenho:
+            //*
+            //**
+            //***
+            //****
+            //*****
+            //******
+            //*******
+            //********
+            //*********
+            //**********
+
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+                {
+                    Console.Write("*");
+                    if (contadorColuna >= contadorLinha)
+                        break;                    
+                }
+                Console.WriteLine();
+            }
+
+            //Essa é uma maneira diferente de escrever o mesmo código sem o break
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna <= contadorLinha; contadorColuna++)                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ P13-ForEncadeado/TreinandoWhileEFor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreinandoWhileEFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Treinando While e For");
+            
+            //Exercício For
+            Console.WriteLine("Escreva um laço for que imprima os números de 0 a 10");
+            for (int contadorFor = 0; contadorFor <= 10; contadorFor ++)
+                Console.WriteLine(contadorFor);
+
+            //Exercício While
+            Console.WriteLine("Escreva um laço while que imprima os números de 0 a 10");
+            int contadorWhile = 0;
+            while (contadorWhile <= 10)
+            {
+
+                Console.WriteLine(contadorWhile);
+                contadorWhile++;
+            }
+
+                Console.ReadLine();
+        }
+    }
+}
