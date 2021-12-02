10e98a8e9bc8cd3db648d028a29fc7645e379282
VivianeAntonio <asilvaviviane@gmail.com>
2021-12-02 12:49:23 -0300

    rearranging folders

---
+++ Parte 1/3-CriandoVariaveisPontoFlutuante/TestaPontoFlutuante/TestaPontoFlutuante/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestaPontoFlutuante
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testando ponto flutuante");
+            double salario = 1250.70;
+            Console.WriteLine("Meu salário é " + salario);
+
+            double valor = 5.0/2;
+            Console.WriteLine(valor);
+
+            Console.ReadLine();
+        }
+    }
+}
