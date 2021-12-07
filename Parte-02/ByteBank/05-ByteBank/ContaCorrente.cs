ea6b1b373d5577ef720aba3b01bd2a19ede68c60
VivianeAntonio <asilvaviviane@gmail.com>
2021-12-07 11:39:36 -0300

    entendendo namespaces

---
+++ Parte-02/ByteBank/05-ByteBank/ContaCorrente.cs
@@ -0,0 +1,45 @@
+﻿// using _05_ByteBank;
+
+namespace _05_ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente titular;
+        public int agencia;
+        public int numero;
+        public double saldo = 100;
+
+        // a função var ter um return do tipo bool
+        public bool Sacar(double valor)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+
+            this.saldo -= valor;
+            return true;
+
+        }
+
+        // void é uma função sem retorno
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+
+            this.saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+
+        }
+    }
+}
+
