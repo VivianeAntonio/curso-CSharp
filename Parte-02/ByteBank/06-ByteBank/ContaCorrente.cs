5fcaeb0e3b4e722ebc63e068a1373f0d89bc30ac
VivianeAntonio <asilvaviviane@gmail.com>
2021-12-16 19:22:49 -0300

    classe privada

---
+++ Parte-02/ByteBank/06-ByteBank/ContaCorrente.cs
@@ -0,0 +1,61 @@
+﻿// using _05_ByteBank;
+
+namespace _06_ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente titular;
+        public int agencia;
+        public int numero;
+        private double saldo = 100;
+
+        public void DefinirSaldo(double saldo)
+        {
+            if (saldo < 0)
+            {
+                return;
+            }
+
+            this.saldo = saldo;
+        }
+
+        //criando método
+        public double ObterSaldo()
+        {
+            return saldo;
+        }
+
+        // a função var ter um return do tipo bool
+        public bool Sacar(double valor)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+
+            saldo -= valor;
+            return true;
+
+        }
+
+        // void é uma função sem retorno
+        public void Depositar(double valor)
+        {
+            saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+
+            saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+
+        }
+    }
+}
+
