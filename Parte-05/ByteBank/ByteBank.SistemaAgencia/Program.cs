7479056aa1fbcf0dbab0ac36be2c4590a62736c1
VivianeAntonio <asilvaviviane@gmail.com>
2022-01-13 07:55:12 -0300

    referencias nuget

--- Parte-05/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ Parte-05/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ByteBank.Modelos;
 using ByteBank.Modelos.Funcionarios;
+using Humanizer;
 
 namespace ByteBank.SistemaAgencia
 {
@@ -12,11 +13,17 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(256, 85648);
+            DateTime dataFimPagamento = new DateTime(2022, 02 , 28);
+            DateTime dataCorrente = DateTime.Now;
 
-            new ContaCorrente(156, 8745);
+            Console.WriteLine(dataFimPagamento);
+            Console.WriteLine(dataCorrente);
 
-            Console.WriteLine(conta.Numero);
+            TimeSpan diferenca = TimeSpan.FromMinutes(60);
+
+            string mensagem = "Vencimento em: " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+            
+            Console.WriteLine(mensagem);
 
             Console.ReadLine();
         }
